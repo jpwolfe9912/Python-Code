7bd8abe4d2e2cfe1658fdf0f0eaa73a18edf1646
Jeremy Wolfe <jpwolfe@me.com>
2023-03-10 12:21:43 -0500

    Added inheritance

--- test.py
+++ test.py
@@ -46,9 +46,34 @@
 # student = Student()
 # print(student.average())
 
-class ClassTest:
-    def instance_method(self):
-        print(f"Called method of {self}")
+# class ClassTest:
+#     def instance_method(self):
+#         print(f"Called method of {self}")
 
-test = ClassTest()
-print(test.instance_method())
+# test = ClassTest()
+# print(test.instance_method())
+
+## Class Inheritance
+# class Person:
+#     def __init__(self, fname, lname) -> None:
+#         self.firstname = fname
+#         self.lastname = lname
+
+#     def printname(self):
+#         print(self.firstname, self.lastname)
+
+# x = Person("John", "Doe")
+# x.printname()
+
+# class Student(Person):
+#     def __init__(self, fname, lname, year):   # no longer inherits properties of parent
+#         super().__init__(fname, lname)
+#         self.graduationyear = year
+
+#     def welcome(self):
+#         print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+
+# x = Student("Mike", "Olsen", 2022)
+# x.welcome()
+
